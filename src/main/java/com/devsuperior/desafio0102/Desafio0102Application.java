22c0f2df70f9939ab7610958754e3a24b49a52c4
Thiago Santos <thiagop.santos182@gmail.com>
2023-10-30 16:20:13 -0400

    first commit

---
+++ src/main/java/com/devsuperior/desafio0102/Desafio0102Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafio0102;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio0102Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio0102Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/desafio0102/Desafio0102ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafio0102;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio0102ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
